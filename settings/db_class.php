d79b7a6f58db956584c0e594bd57c8c4b91d5709
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-02-01 13:21:07 +0000

    db

--- settings/db_class.php
+++ settings/db_class.php
@@ -2,65 +2,199 @@
 //database
 
 //database credentials
-require('db_cred.php');
+require 'dbCred.php';
 
-/**
- *@author David Sampah
- *@version 1.1
- */
 class db_connection
 {
-	//properties
-	
+    //properties
+    public $db = null;
+    public $results = null;
 
-	//connect
-	/**
-	*Database connection
-	*@return bolean
-	**/
-	
+    //connect
+    /**
+     *Database connection
+     *@return bolean
+     **/
+    function db_connect()
+    {
+        //connection
+        $this->db = mysqli_connect(SERVER, USERNAME, PASSWD, DATABASE);
 
-	//execute a query
-	/**
-	*Query the Database
-	*@param takes a connection and sql query
-	*@return bolean
-	**/
-	
+        //test the connection
+        if (mysqli_connect_errno()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    //execute a query
+    /**
+     *Query the Database
+     *@param takes a connection and sql query
+     *@return bolean
+     **/
+    function db_query($sqlQuery)
+    {
+        if (!$this->db_connect()) {
+            return false;
+        } elseif ($this->db == null) {
+            return false;
+        }
+        //run query
+        $this->results = mysqli_query($this->db, $sqlQuery);
 
-	//execute a query with mysqli real escape string
-	//to saveguard from sql injection
-	/**
-	*Query the Database
-	*@param takes a connection and sql query
-	*@return bolean
-	**/
-	
+        if ($this->results == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    //execute a query with mysqli real escape string
+    //to saveguard from sql injection
+    /**
+     *Query the Database
+     *@param takes a connection and sql query
+     *@return bolean
+     **/
+    function db_query_escape_string($sqlQuery)
+    {
+        //run query
+        $this->results = mysqli_query($this->db, $sqlQuery);
 
-	//fetch a data
-	/**
-	*get select data
-	*@return a record
-	**/
-	
+        if ($this->results == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    //fetch a data
+    /**
+     *get select data
+     *@return a record
+     **/
+    function db_fetch_one()
+    {
+        //check if result was set
+        if ($this->results == null) {
+            return false;
+        } elseif ($this->results == false) {
+            return false;
+        }
+        //return a record
+        return mysqli_fetch_assoc($this->results);
+    }
 
-	//fetch all data
-	/**
-	*get select data
-	*@return all record
-	**/
-	
+//////////////////// EXPERIMENTAL /////////////////////////
 
+    // method to fetch one row from database (select)
+	function fetchOne($sqlQuery){
 
-	//count data
-	/**
-	*get select data
-	*@return a count
-	**/
-	
-	
+		// if query executes successfully
+		if($this->db_query($sqlQuery)) {
+			// return one row
+			return mysqli_fetch_assoc($this->results);
+		}
+		// else return false
+		return false;
+	}
+    
+
+    // method to fetch multiple rows from database (select)
+	function fetch($sqlQuery){
+
+		// if query executes successfully
+		if($this->db_query($sqlQuery)) {
+			// return all the rows
+			return mysqli_fetch_all($this->results, MYSQLI_ASSOC);
+		}
+		// else return false
+		return false;	
+	}
+
+//////////////////////////////////////////////////////////////
+
+    //fetch all data
+    /**
+     *get select data
+     *@return all record
+     **/
+    function db_fetch_all()
+    {
+        //check if result was set
+        if ($this->results == null) {
+            return false;
+        } elseif ($this->results == false) {
+            return false;
+        }
+
+        //return a record
+        return mysqli_fetch_all($this->results, MYSQLI_ASSOC);
+    }
+
+    //count data
+    /**
+     *get select data
+     *@return a count
+     **/
+    function db_count()
+    {
+        //check if result was set
+        if ($this->results == null) {
+            return false;
+        } elseif ($this->results == false) {
+            return false;
+        }
+
+        //return a record
+        return mysqli_num_rows($this->results);
+    }
+
+    //execute a query that returns the just inserted id
+    /**
+     *Query the Database
+     *@param takes a connection and sql query
+     *@return returns the just inserted id
+     **/
+    function db_query_id($sqlQuery)
+    {
+        if (!$this->db_connect()) {
+            return false;
+        } elseif ($this->db == null) {
+            return false;
+        }
+
+        //run query
+        $this->results = mysqli_query($this->db, $sqlQuery);
+
+        if ($this->results == false) {
+            return false;
+        } else {
+            //get the just inserted id
+            return mysqli_insert_id($this->db);
+        }
+    }
+
+    //execute a query with mysqli real escape string and return the just inserted id
+    //to saveguard from sql injection
+    /**
+     *Query the Database
+     *@param takes a connection and sql query
+     *@return bolean
+     **/
+    function db_query_escape_string_id($sqlQuery)
+    {
+        //run query
+        $this->results = mysqli_query($this->db, $sqlQuery);
+
+        if ($this->results == false) {
+            return false;
+        } else {
+            //get the just inserted id
+            return mysqli_insert_id($this->db);
+        }
+    }
 }
 ?>
--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,5 +1,9 @@
 <?php
 //Database credentials
-
+//for XAMPP
+define("DATABASE", "Dash");
+define("SERVER", "localhost");
+define("USERNAME", "root");
+define("PASSWD", "");
 
 ?>
