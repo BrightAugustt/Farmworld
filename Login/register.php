1ed7d2fd1702a11d7048398e55c5ef8c9b7bb627
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-02-05 17:03:43 +0000

    frontend for register

--- Login/register.php
+++ Login/register.php
@@ -1,7 +1,7 @@
 <?php
+
 session_start();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,62 +9,55 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Page</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/register.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-
-    <div class="flex-container">
-        <div class="info">
-            <h3>Welcome to Farm-a-World</h3>
-            <p>
-            Lorem ipsum dolor sit amet consectetur. Maecenas aliquam nulla integer et dictumst leo consequat dapibus orci. Ut porta aliquet cursus sit lorem. elerisque in sapien dignissim velit amet Lorem ipsum dolor sit amet sectetur. Maecenas aliquam nulla integer et dictumst leo consequat dapibus orci. Ut porta aliquet cursus sit lorem. Scelerisque in sapien dignissim velit amet 
-            </p>
-        </div>
-
+    <div class="container">
         <div class="form">
-            <h3>Create an account</h3>
-            <form action="" method="POST" id="formId">
-            <div class="form-floating">
-                <label>Your First Name</label>
-                <input type="text" name="customer_fname" id="customer_fname" class="form-control">
-            </div>
-            <div class="form-floating">
-                <label>Your Last Name</label>
-                <input type="text" name="customer_lname" name="customer_lname" class="form-control">
-            </div>
-            <div class="form-floating">
-                <label>Contact Number</label>
-                <input type="text" name="customer_contact" id="customer_contact" class="form-control">
-            </div>
-            <div class="form-floating">
-                <label>Region</label>
-                <input type="text" name="customer_region" id="customer_region" class="form-control">
-            </div>
-            <div class="form-floating">
-                <label>Email address</label>
-                <input type="text" name="customer_email" id="customer_email" class="form-control">
-            </div>
-            <div class="form-floating">
-                <label>Password</label>
-                <input type="password" name="customer_pass" id="customer_pass" class="form-control">
-            </div>
+            <h2>Create Account</h2>
+            <form id="formid" action="" method="POST">
+                <div class="form-floating">
+                    <label>Your First Name</label>
+                    <input type="text" name="customer_fname" id="customer_fname" class="form-control" placeholder="First Name">
+                </div>
+                <div class="form-floating">
+                    <label>Your Last Name</label>
+                    <input type="text" name="customer_lname" name="customer_lname" class="form-control" placeholder="Last Name">
+                </div>
+                <div class="form-floating">
+                    <label>Contact Number</label>
+                    <input type="text" name="customer_contact" id="customer_contact" class="form-control" placeholder="Contact">
+                </div>
+                <div class="form-floating">
+                    <label>Region</label>
+                    <input type="text" name="customer_region" id="customer_region" class="form-control" placeholder="Region">
+                </div>
+                <div class="form-floating">
+                    <label>Email address</label>
+                    <input type="text" name="customer_email" id="customer_email" class="form-control" placeholder="Email Address">
+                </div>
+                <div class="form-floating">
+                    <label>Password</label>
+                    <input type="password" name="customer_pass" id="customer_pass" class="form-control" placeholder="Password">
+                </div>
 
-            <div class="button">
-            <button class="button1" id="butcustomer" >Sign Up as a customer</button>
-            <p>OR</p>
-            <button class="button2" id="butaeo">Sign Up as an AEO</button>
-            </div>
+                <div class="button">
+                <button class="button1" id="butcustomer" >Sign up as a customer</button>
+                <p>OR</p>
+                <button class="button2" id="butaeo">Sign up as an AEO</button>
+                </div>
 
-            <div class="already">
-                <p>Already have an account?<span><a href="login.php">Login Here</a></span></p>
-            </div>
-        
+                <div class="already">
+                    <p>Already have an account? <span class="login"><a href="login.php" class="login">Login Here</a></span></p>
+                </div>
             </form>
-
         </div>
     </div>
+
+
     <script>
         $("#butcustomer").click(function(ev) {
 			  var form = $("#formId");
