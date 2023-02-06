2a172e0d31d6eba41535d2cb2e7a6f660326ed3e
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-02-06 11:38:24 +0000

    ajax

--- Login/register.php
+++ Login/register.php
@@ -66,28 +66,20 @@ session_start();
     <script>
         $("#butcustomer").click(function(ev) {
 			  var form = $("#formId");
-			  var fname = $("#fname").val();
-			  var fnumber = $("#fnumber").val();
-			  var url = form.attr('./actions/adduser.php');
-			  if (fname!="" && fnumber!=""){
+			  var url = form.attr('./actions/addCustomer.php');
 				$.ajax({
 				  type: "POST",
 				  url: url,
 				  data: form.serialize(),
 				  success: function(data) {
-						
 					// Ajax call completed successfully
 					alert("Form Submited Successfully");
 				  },
 				  error: function(data) {
-						
 					  // Some error in ajax call
 					  alert("some Error");
 				  }
 			  });
-			  }else{
-				alert("Please fill all the flield");
-			  }
 			  
 		  });
 
