db5a6371ee3199e2ff0c642cc4a8551dbef0375e
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-02-06 23:59:24 +0000

    home

--- Ecom/homepage.php
+++ Ecom/homepage.php
@@ -22,11 +22,11 @@ $customer_id = isset($_SESSION['customer_id'])? $_SESSION['customer_id']: "";
     <div class="b-example-divider"></div>
 
     <!--Header of Page-->
-    <header class="p-3 mb-2 bg-info text-white">
+    <header class="p-3 mb-2 bg-success text-white">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <img class="bi me-2" src="../images/dashlog.png" width="200" height="32" role="img" aria-label="Bootstrap">
+                <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <img class="bi me-2" src="../images/logo.png" width="200" height="32" role="img" aria-label="Bootstrap">
                     <use xlink:href="#bootstrap" />
                     </img>
                 </a>
