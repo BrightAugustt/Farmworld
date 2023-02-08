d8c6d61e9f514f66878c54552280a76194a479de
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-02-08 10:56:38 +0000

    on it

--- aeo/add_crop.php
+++ aeo/add_crop.php
@@ -16,12 +16,15 @@ session_start();
     <title>AEO Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/aeo.css">
+    <script defer src="activepage.js"></script>
 
 
     <style>
@@ -52,19 +55,20 @@ session_start();
 
 <body>
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="allcrops.php">
-            <img class="bi me-2" src="../images/logo.png" width="189" height="32" role="img" aria-label="Bootstrap">Welcome, Admin!
-            <use xlink:href="#bootstrap" />
+<header class="navbar navbar-dark sticky-top bg-white flex-md-nowrap p-0 shadow header">
+         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="allcrops.php">
+            <img class="bi me-2" src="../images/logo.png" width="189" height="32" role="img" aria-label="Bootstrap">
+                <use xlink:href="#bootstrap" />
             </img>
         </a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
+        <h6 style="color:black;text-align:center;">Welcome AEO Officer</h6>
         <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-                <a class="nav-link px-3" href="../login/logout.php">Sign Out</a>
+            <div class=" text-nowrap admin" >
+                <!-- <a class="nav-link px-3" href="../login/logout.php" style="color:black">Sign Out</a>-->
+                <span id="boot-icon" class="bi bi-person-circle" style="font-size: 30px;"></span>
             </div>
         </div>
     </header>
