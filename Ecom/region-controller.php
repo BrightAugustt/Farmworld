9eb00001630bbae081ce5a55a1bef7610252be2d
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-02-10 00:17:36 +0000

    select

--- Ecom/region-controller.php
+++ Ecom/region-controller.php
@@ -1,3 +1,18 @@
-<?php 
-echo $_POST['region'] ;
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $destination = $_POST['region'];
+
+  switch ($destination) {
+    case 'Greater Accra':
+      header('Location: gAccra.php');
+      break;
+    case 'Eastern':
+      header('Location: eastern.php');
+      break;
+    default:
+      echo 'Invalid destination selected.';
+      break;
+  }
+  exit;
+}
 ?>
