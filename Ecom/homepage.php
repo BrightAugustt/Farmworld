ba519e2ae8febfa6e37b9c00dfac07c25a7fbbbe
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-02-11 16:46:30 +0000

    home

--- Ecom/homepage.php
+++ Ecom/homepage.php
@@ -80,7 +80,7 @@ $customer_id = isset($_SESSION['customer_id']) ? $_SESSION['customer_id'] : "";
             <div class="w-100 h-50" style="background: linear-gradient(160.29deg,#00b53f .67%,#00831e 100.93%)">
             <div style="display:flex; flex-direction:column; justify-content:space-around; height:100%">
                 <div class="row height d-flex justify-content-center align-items-center ">
-                    <div style="display: flex; flex-direction:row; width:100%; justify-content:center;margin-bottom:-120px;">
+                    <div style="display: flex; flex-direction:row; width:100%; justify-content:center;margin-bottom:-150px;">
                         <span class=" mb-5" style="color:white; text-align:center"> Find any crop in...
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="success" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
