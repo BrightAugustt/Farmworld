d9121ac4e7481950bc43cc3da21558739ff398d8
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-03-14 21:06:47 +0000

    PAY

--- payment/paybox.php
+++ payment/paybox.php
@@ -19,6 +19,96 @@ $transaction_id = $_POST['transaction_id'];
 $status = $_POST['status'];
 
 ?>
+
+<!-- Create a PayBox account and obtain your API credentials (merchant ID and secret key).
+
+Download and install the PayBox SDK by following the instructions provided by PayBox. This will involve downloading a PHP library and 
+installing it on your web server.
+
+Configure your website to communicate with the PayBox API using the SDK. Here's an example of how you could do this: -->
+<?php
+require_once 'paybox/PayBox.php'; // Include the PayBox PHP library
+
+$merchantId = 'your-merchant-id'; // Replace with your actual merchant ID
+$secretKey = 'your-secret-key'; // Replace with your actual secret key
+
+$paybox = new PayBox($merchantId, $secretKey); // Initialize the PayBox object
+
+// Set the PayBox API endpoint (sandbox or live)
+$isTestMode = true; // Change to false when you're ready to go live
+$paybox->setTestMode($isTestMode);
+
+// Optionally, set other PayBox configuration options
+$paybox->setLanguage('en'); // Set the language to use for PayBox error messages
+
+// Handle a payment request from the user
+if (isset($_POST['payment_amount'])) {
+    $paymentAmount = $_POST['payment_amount']; // Get the payment amount from the user
+    $paymentDescription = 'Payment for E-Commerce order'; // Set a description for the payment
+
+    // Generate a unique payment reference
+    $paymentReference = uniqid();
+
+    // Build the payment request data
+    $paymentData = array(
+        'amount' => $paymentAmount,
+        'description' => $paymentDescription,
+        'reference' => $paymentReference,
+    );
+
+    // Send the payment request to PayBox
+    $response = $paybox->initiatePayment($paymentData);
+
+    // Check if the payment request was successful
+    if ($response['status'] == 'success') {
+        // Redirect the user to the PayBox payment page
+        header('Location: ' . $response['redirect']);
+        exit;
+    } else {
+        // Handle the payment request failure
+        echo 'Payment request failed: ' . $response['message'];
+    }
+}
+?>
+
+<!-- In this example, we're checking for the payment_reference parameter in the URL, which is provided by PayBox in the redirect URL after a 
+payment is made. We're then using the checkPaymentStatus() method to verify the payment status with PayBox. If the payment was successful, 
+we're handling it appropriately, and if it failed, we're displaying an error message.
+
+Note that this is just a basic example, and you may need to modify it to suit your specific requirements. Additionally, you'll need to 
+ensure that the payment_reference parameter is passed correctly in the redirect URL from PayBox to your website. -->
+<?php
+require_once 'paybox/PayBox.php'; // Include the PayBox PHP library
+
+$merchantId = 'your-merchant-id'; // Replace with your actual merchant ID
+$secretKey = 'your-secret-key'; // Replace with your actual secret key
+
+$paybox = new PayBox($merchantId, $secretKey); // Initialize the PayBox object
+
+// Set the PayBox API endpoint (sandbox or live)
+$isTestMode = true; // Change to false when you're ready to go live
+$paybox->setTestMode($isTestMode);
+
+// Handle the payment response from PayBox
+if (isset($_GET['payment_reference'])) {
+    $paymentReference = $_GET['payment_reference']; // Get the payment reference from the PayBox response
+    $paymentData = array(
+        'reference' => $paymentReference,
+    );
+
+    // Check if the payment was successful
+    $response = $paybox->checkPaymentStatus($paymentData);
+    if ($response['status'] == 'success' && $response['data']['status'] == 'successful') {
+        // Handle the successful payment
+        echo 'Payment was successful!';
+    } else {
+        // Handle the failed payment
+        echo 'Payment failed: ' . $response['message'];
+    }
+}
+?>
+
+
 <!-- Next, you will need to create a form that allows customers to select PayBox as a payment option and initiate transactions. 
 This form should include the necessary input fields, such as the customer's name, email address, order amount, and a unique transaction ID. -->
 <form method="post" action="https://www.paybox.com.gh/payment">
