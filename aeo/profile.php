75bf77d400ef8014ea3733185b6f50c5f4da6f97
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-04-01 12:46:30 +0000

    ok

--- aeo/profile.php
+++ aeo/profile.php
@@ -136,13 +136,13 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
                 <div class="container rounded bg-white mt-5 mb-5">
                     <div class="row">
                         <div class="col-md-3 border-right">
-                            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" 
-                            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
-                            <button type='button' class='mr-3 btn-first-modal btn btn-outline-success btn-lg' data-toggle='modal' data-target='#first-modal$i' style='font-size:10px;'>
-                            <span class='bi bi-card-image'></span></button>
-                            <span class="font-weight-bold">Edogaru</span>
-                            <span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-                            
+                            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                                <button type='button' class='mr-3 btn-first-modal btn btn-outline-success btn-lg' data-toggle='modal' data-target='#first-modal$i' style='font-size:10px;'>
+                                    <span class='bi bi-card-image'></span></button>
+                                <span class="font-weight-bold">Edogaru</span>
+                                <span class="text-black-50">edogaru@mail.com.my</span><span> </span>
+                            </div>
+
                         </div>
                         <div class="col-md-5 border-right">
                             <div class="p-3 py-5">
@@ -158,9 +158,9 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
                                     <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
                                     <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
                                     <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                                    <div class="col-md-12"><label class="labels">Region</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
                                     <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
+                                    <div class="col-md-12"><label class="labels">Email Address</label><input type="text" class="form-control" placeholder="enter email address" value=""></div>
                                     <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
                                 </div>
                                 <div class="row mt-3">
@@ -168,6 +168,12 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
                                     <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
                                 </div>
                                 <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="p-3 py-5">
+                                <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
+                                <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
                             </div>
                         </div>
                     </div>
