e010030b5613330c14113633ace53c80e574b2c0
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-04-15 22:56:58 +0000

    k

--- view/shopping-cart.php
+++ view/shopping-cart.php
@@ -270,8 +270,7 @@ $total = 0;
                                                 </div>
                                             </td>
                                             <td class='shoping__cart__total'>
-                                                $subtotal ={$cart['qty']} * {$cart['crop_price']}
-                                                <?php echo $subtotal; ?>
+                                                $subtotal
                                             </td>
                                             <td class='shoping__cart__item__close'>
                                                 <a href='../actions/deletefromCart.php?crop_id={$cart['crop_id']}'data-toggle='tooltip'><button class='removebutton'><span><i class='fa fa-trash-o'></i></span></button></a>
