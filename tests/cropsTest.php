5224beff54b006a7b20846ccef670e85594ba2cd
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-04-15 04:02:14 +0100

    testing

---
+++ tests/cropsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class CropsTest extends \PHPUnit\Framework\TestCase{
+
+    public function testAdd{
+        $crop = new App\crops;
+        $result = $crop->insert();
+
+        $this->assertTrue($result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
