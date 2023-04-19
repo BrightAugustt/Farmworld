93c69d1edeef0b777c9fdaf380506da7ef0ac637
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-04-19 20:21:37 +0100

    try

--- actions/addtoCart.php
+++ actions/addtoCart.php
@@ -12,11 +12,44 @@ if (isset($_POST['addcart'])) {
     $custId = $_SESSION['customer_id'];
     $qty = $_POST['qty'];
 
-    $addCart=add_to_cart_ctr($prodId, $ip_address, $custId, $qty);
-    if($addCart == True){
-        header('Location:../view/index.php');
+    if(isset($_SESSION['customer_id'])){
+        $custId=$_SESSION['customer_id'];
+        $addCart=add_to_cart_ctr($prodId, $ip_address, $custId, $qty);
+        if($addCart == True){
+            header('Location:../view/index.php');
+        }else{
+            echo"Failed insertion!";
+        }
+
+    }else{
+        if(!isset($_SESSION['cart'])){
+            $_SESSION['cart']=array();
+        }
+        $item = array(
+            'crop_id'=>$prodId,
+            'qty'=>$qty
+        );
     }
+    array_push($_SESSION['cart'],$item);
+    header('Location:../view/index.php');
+
+    // $addCart=add_to_cart_ctr($prodId, $ip_address, $custId, $qty);
+    // if($addCart == True){
+    //     header('Location:../view/index.php');
+    // }
 }
+
+// if (isset($_POST['addcart'])) {
+//     $ip_address = $_SERVER["REMOTE_ADDR"];
+//     $prodId = $_POST['crop_id'];
+//     $custId = $_SESSION['customer_id'];
+//     $qty = $_POST['qty'];
+
+//     $addCart=add_to_cart_ctr($prodId, $ip_address, $custId, $qty);
+//     if($addCart == True){
+//         header('Location:../view/index.php');
+//     }
+// }
 
 if (isset($_POST['addtocart'])) {
     $ip_address = $_SERVER["REMOTE_ADDR"];
@@ -30,43 +63,6 @@ if (isset($_POST['addtocart'])) {
     }
 }
 
-
-
-
-// if (isset($_POST['addcart'])) {
-//     $ip_address = $_SERVER["REMOTE_ADDR"];
-//     $prodId = $_POST['crop_id'];
-//     $custId = $_SESSION['customer_id'];
-//     $qty = $_POST['qty'];
-
-//     // print_r($ip_address);
-//     // print_r($prodId);
-//     // print_r($custId);
-//     // print_r($qty);
-//     // exit();
-
-//     // call function to check if the user has added an item to the cart.
-//     $duplicate_check = dup_cart_qty_ctr($prodId, $custId);
-
-//     // check if there is no input else add to cart.
-//     if ($duplicate_check == false) {
-//         $addCart = add_to_cart_ctr($prodId, $ip_address, $custId, $qty);
-//         if ($addCart) {
-
-//             header('Location: ../view/index.php');
-//         } else {
-//             echo "Failed insertion!";
-//         }
-//      } else {
-//         $update_check = update_cart_qty_ctr($prodId, $custId, $qty);
-//         if ($update_check) {
-//             // if update was succesfull, redirect to cart page.
-//             header('location:../view/shopping-cart.php');
-//         } else {
-//             echo "Failed Update!";
-//         }
-//     }
-// }
 
 
 ?>
