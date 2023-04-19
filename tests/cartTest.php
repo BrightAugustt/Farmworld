402d7665e644858c5fd068f98ad8afa2080f5580
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2023-04-19 19:41:58 +0100

    add

--- tests/cartTest.php
+++ tests/cartTest.php
@@ -20,29 +20,15 @@ class CartTest extends \PHPUnit\Framework\TestCase{
 
    }
 
-   public function testUpdate_cart_items(){
-     $update_cart_qty_cls = new cart_class();
-     $result = $update_cart_qty_cls->update_cart_qty_cls(5,3);
+//    public function testUpdate_cart_items(){
+//      $update_cart_qty_cls = new cart_class();
+//      $result = $update_cart_qty_cls->update_cart_qty_cls(5,3);
 
-     $this->assertTrue($result);
+//      $this->assertTrue($result);
 
-}
-
-// public function testInsert_Order_Details(){
-//      $insert_order_cls = new cart_class();
-//      $custId = 1;
-//     $cropName = "Tomatoes";
-//     $date = "2023-04-13";
-//     $qty = 5;
-//     $customerEmail = "johndoe@example.com";
-//     $amount = 15.99;
-//     $sql = "INSERT INTO `orders` (`customer_id`, `crop_name`, `order_date`, `qty`, `customer_email`, `amount`) VALUES ('$custId', '$cropName', '$date', '$qty', '$customerEmail', '$amount')";
-//     $result = $insert_order_cls->insert_order_cls($custId, $cropName, $date, $qty, $customerEmail, $amount);
-//     $this->assertTrue($result);
 // }
 
 
+
+
 }
-
-
-?>
