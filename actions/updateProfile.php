e68550f391c3c91ac04eece286ab5c98117df4c7
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-04-19 17:17:25 +0000

    d

--- actions/updateProfile.php
+++ actions/updateProfile.php
@@ -20,7 +20,7 @@
 include("../controllers/contact_controller.php");
 
 if (isset($_POST['updateBtn'])) {
-    $id = $_POST['customer_id'];
+    $cid = $_POST['customer_id'];
     $fname = $_POST['customer_fname'];
     $lname = $_POST['customer_lname'];
     $number = $_POST['customer_contact'];
@@ -30,19 +30,19 @@ if (isset($_POST['updateBtn'])) {
     if ($result) {
         // retrieve updated customer data from database
         $customer = get_one_record_ctr($cid);
-        $customer_id = $customer['customer_id'];
-        $customer_fname = $customer['customer_fname'];
-        $customer_lname = $customer['customer_lname'];
-        $customer_contact = $customer['customer_contact'];
-        $customer_email = $customer['customer_email'];
+        $cid = $customer['customer_id'];
+        $fname = $customer['customer_fname'];
+        $lname = $customer['customer_lname'];
+        $number = $customer['customer_contact'];
+        $email = $customer['customer_email'];
 
         // display updated customer data on page
-        echo '<h2>Customer Details Updated:</h2>';
-        echo '<p>ID: ' . $customer_id . '</p>';
-        echo '<p>First Name: ' . $customer_fname . '</p>';
-        echo '<p>Last Name: ' . $customer_lname . '</p>';
-        echo '<p>Contact Number: ' . $customer_contact . '</p>';
-        echo '<p>Email: ' . $customer_email . '</p>';
+        // echo '<h2>Customer Details Updated:</h2>';
+        // echo '<p>ID: ' . $customer_id . '</p>';
+        // echo '<p>First Name: ' . $customer_fname . '</p>';
+        // echo '<p>Last Name: ' . $customer_lname . '</p>';
+        // echo '<p>Contact Number: ' . $customer_contact . '</p>';
+        // echo '<p>Email: ' . $customer_email . '</p>';
     } else {
         echo 'error';
     }
